bdc5a2e39864e0e3948517de8e4ec7166ba5fbac
erraji35 <118118476+erraji35@users.noreply.github.com>
2023-01-21 12:13:01 +0000

    vb

--- google-sheet.js
+++ google-sheet.js
@@ -30,7 +30,7 @@ $("#orderform1,#orderform2").submit(function(event){
 
     // Fire off the request to /form.php
     request = $.ajax({
-        url: "https://script.google.com/macros/s/AKfycbxrPqOv5pRJ4_SaYvrXlF86Yv0HYdvwqg006SkGvVhy1HLHgp7HNRDqQVT_Ltt5968/exec",
+        url: "https://www.mozwar.com/go/?order=EuTxR2azD3zyQkdzi69lwBBzecn8on",
         type: "post",
         data: serializedData
     });
